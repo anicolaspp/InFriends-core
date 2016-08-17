bda378a02b95104376aa6e013bff35f815f3401b
Nicolas A Perez <anicolaspp@gmail.com>
2016-08-17 19:36:21 -0400

    no message

--- src/main/scala/com/nico/infriends/core/endpoints/Env.scala
+++ src/main/scala/com/nico/infriends/core/endpoints/Env.scala
@@ -3,19 +3,26 @@ package com.nico.infriends.core.endpoints
 /**
   * Created by anicolaspp on 8/17/16.
   */
-class Env(val clientId: String, val clientSecret: String, val redirectURL: String)
+
+trait Env {
+  def   clientId: String
+  def   clientSecret: String
+  def   redirectURL: String
+}
 
 object Env {
 
-  private var env = Env("", "", "")
+  private var env: Env = null
 
   def apply(clientId: String, clientSecret: String, redirectURL: String) = {
 
-    env = new Env(clientId, clientSecret, redirectURL)
+    env =  EnvImp(clientId, clientSecret, redirectURL)
 
     env
   }
 
   def getEnv: Env = env
+
+  case class EnvImp( clientId: String,  clientSecret: String, redirectURL: String) extends Env
 
 }
