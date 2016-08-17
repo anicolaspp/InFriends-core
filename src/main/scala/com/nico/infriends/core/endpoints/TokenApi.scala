7c0563033e58288ea92763b416122a159420ce13
Nicolas A Perez <anicolaspp@gmail.com>
2016-08-17 19:48:24 -0400

    no message

--- src/main/scala/com/nico/infriends/core/endpoints/TokenApi.scala
+++ src/main/scala/com/nico/infriends/core/endpoints/TokenApi.scala
@@ -17,10 +17,10 @@ trait TokenApi {
     println(code)
 
     val res = scalaj.http.Http("https://api.instagram.com/oauth/access_token").postForm(Seq(
-      "client_id"     ->  Env.getEnv.clientId,
-      "client_secret" ->  Env.getEnv.clientSecret,
+      "client_id"     ->  "3e4dff94fc1e42c99544d271113a3773",
+      "client_secret" ->  "4140e98fb6ab4f31bca333b1ab63cf64",
       "grant_type"    ->  "authorization_code",
-      "redirect_uri"  ->  Env.getEnv.redirectURL,
+      "redirect_uri"  ->  "http://infriends-core.herokuapp.com/push",
       "code"          ->  code
     )).asString
 
